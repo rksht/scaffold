3cbfb764e9dafabb54c4fddd36ca9a0312fddd58
Soumik Rakshit <soumikrakshit@yandex.com>
2016-08-04 19:55:05 +0530

    Keeping the two open-addressed tables.
    Will clean up now.

--- tests/hash_soa_test.cpp
+++ tests/hash_soa_test.cpp
@@ -1,5 +1,8 @@
-/// Testing the performance of the chaining-based hash table vs a quadratic hash
-/// table. The code is in a C-style that doesn't use member functions.
+/// Testing the performance of the chaining-based hash table vs a quadratic
+/// hash table. The code is in a C-style that doesn't use member functions.
+/// This one uses 2 arrays, one for keys and one for values. This should yield
+/// less cache misses because we only look into the keys of the entries during
+/// lookup.
 
 #include "hash.h"
 #include "array.h"
@@ -43,23 +46,31 @@ template <> struct ToAdd<ProbeLinear> {
     static uint64_t to_add(uint64_t last) { return last + 1; }
 };
 
-/// The Open-addressed hash-table representation
+/// The Open-addressed hash-table representation. We use two arrays, one for
+/// the keys and one for the corresponding values. Both always have the same
+/// size. The values array is _not_ kept compressed. So the table is good when
+/// you have a small number of key-value pairs to store.
 template <typename T, typename Probe> struct ProbedHash {
     /// Don't support non-POD types
     static_assert(std::is_trivially_copyable<T>::value,
                   "ProbedHash only supports POD-types as values!");
 
-    /// The array used to store entries
-    Array<std::pair<uint64_t, T>> array;
+    /// The array used to store keys
+    Array<uint64_t> _keys;
+    /// And the array for corresponding values
+    Array<T> values;
 
     /// Number of valid entries in the array
     uint64_t _num_entries;
 
-    ProbedHash(Allocator &a, uint64_t starting_size)
-        : array{a}, _num_entries{0} {
-        array::resize(array, clip_to_power_of_2(starting_size));
-        for (auto &e : array) {
-            e.first = TOMBSTONE;
+    ProbedHash(Allocator &keys_alloc, Allocator &values_alloc,
+               uint64_t starting_size)
+        : _keys{keys_alloc}, values{values_alloc}, _num_entries{0} {
+        starting_size = clip_to_power_of_2(starting_size);
+        array::resize(_keys, clip_to_power_of_2(starting_size));
+        array::resize(values, clip_to_power_of_2(starting_size));
+        for (uint64_t &key : _keys) {
+            key = TOMBSTONE;
         }
     }
 };
@@ -70,26 +81,28 @@ namespace probed_hash::_internal {
 /// True if number of entries >= 1/2 of the array size.
 template <typename T, typename Probe>
 inline bool should_rehash(const ProbedHash<T, Probe> &h) {
-    return array::size(h.array) <= 2 * h._num_entries;
+    return array::size(h._keys) <= 2 * h._num_entries;
 }
 
 /// Allocates new array and copies elements there. Deallocates previous array.
 template <typename T, typename Probe>
 void rehash(ProbedHash<T, Probe> &h, uint32_t new_size) {
     new_size = clip_to_power_of_2(new_size);
-    ProbedHash<T, Probe> new_hash{*h.array._allocator, new_size};
+    ProbedHash<T, Probe> new_hash{*h._keys._allocator, *h.values._allocator,
+                                  new_size};
 
-    for (uint64_t i = 0; i < array::size(h.array); ++i) {
-        new_hash.array[i].first = TOMBSTONE;
+    for (uint64_t i = 0; i < array::size(h._keys); ++i) {
+        new_hash._keys[i] = TOMBSTONE;
     }
 
     // Reinsert already inserted keys
-    for (auto &entry : h.array) {
-        if (entry.first != TOMBSTONE) {
-            probed_hash_set(new_hash, entry.first, entry.second);
+    for (uint64_t i = 0; i < array::size(h._keys); ++i) {
+        if (h._keys[i] != TOMBSTONE) {
+            probed_hash_set(new_hash, h._keys[i], h.values[i]);
         }
     }
-    std::swap(h.array, new_hash.array);
+    std::swap(h._keys, new_hash._keys);
+    std::swap(h.values, new_hash.values);
 }
 }
 
@@ -99,12 +112,12 @@ uint64_t probed_hash_find(const ProbedHash<T, Probe> &h, uint64_t key) {
     const uint64_t hash = murmur_hash_64(&key, sizeof(key), 0xCAFEBABE);
     uint64_t add = 0;
     uint64_t i = 0;
-    while (i < array::size(h.array)) {
-        uint64_t idx = (hash + add) % array::size(h.array);
-        if (h.array[idx].first == key) {
+    while (i < array::size(h._keys)) {
+        uint64_t idx = (hash + add) % array::size(h._keys);
+        if (h._keys[idx] == key) {
             return idx;
         }
-        if (h.array[idx].first == TOMBSTONE) {
+        if (h._keys[idx] == TOMBSTONE) {
             return TOMBSTONE;
         }
         add = ToAdd<Probe>::to_add(add);
@@ -117,18 +130,18 @@ template <typename T, typename Probe>
 uint64_t probed_hash_set(ProbedHash<T, Probe> &h, uint64_t key, T value) {
     h._num_entries++;
     if (_internal::should_rehash(h)) {
-        _internal::rehash(h, array::size(h.array) * 2);
+        _internal::rehash(h, array::size(h._keys) * 2);
     }
 
     const uint64_t hash = murmur_hash_64(&key, sizeof(key), 0xCAFEBABE);
 
     uint64_t add = 0;
     uint64_t i = 0;
-    while (i < array::size(h.array)) {
-        uint64_t idx = (hash + add) % array::size(h.array);
-        if (h.array[idx].first == TOMBSTONE || h.array[idx].first == key) {
-            h.array[idx].first = key;
-            h.array[idx].second = value;
+    while (i < array::size(h._keys)) {
+        uint64_t idx = (hash + add) % array::size(h._keys);
+        if (h._keys[idx] == TOMBSTONE || h._keys[idx] == key) {
+            h._keys[idx] = key;
+            h.values[idx] = value;
             return idx;
         }
         add = ToAdd<Probe>::to_add(add);
@@ -175,7 +188,6 @@ int main() {
 }
 #endif
 
-// A test with data size = 128 bytes
 int main() {
     using namespace foundation;
     using namespace probed_hash;
@@ -190,6 +202,7 @@ int main() {
         const uint8_t VALUE_MARKER = 'a';
 
         ProbedHash<Obj<128>, ProbeQuadratic> hash{
+            memory_globals::default_allocator(),
             memory_globals::default_allocator(), INITIAL_SIZE};
 
         Obj<128> ob{};
@@ -207,7 +220,7 @@ int main() {
             if (idx == TOMBSTONE) {
                 log_info("Have TOMBSTONE at key = %lu", i);
             } else {
-                assert(hash.array[idx].second.buf[0] == VALUE_MARKER);
+                assert(hash.values[idx].buf[0] == VALUE_MARKER);
             }
         }
     }
