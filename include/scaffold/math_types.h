a91f00fb99d9a69c3c4d525d879eb3317d416d07
Soumik Rakshit <soumikrakshit@yandex.com>
2016-12-26 14:22:16 +0530

    A few constructors in math types to make life easier.

--- include/scaffold/math_types.h
+++ include/scaffold/math_types.h
@@ -2,18 +2,55 @@
 
 #include "types.h"
 
+#include <emmintrin.h>
+
 namespace foundation {
 struct Vector2 {
     float x, y;
 };
 
+struct Vector4;
+
 struct Vector3 {
     float x, y, z;
+
+    Vector3() = default;
+
+    /// Constructs a Vector3 from a Vector4 by simply ignoring the w
+    /// coordinate. A bit of implicit behavior here, but let' see how it goes.
+    constexpr Vector3(const Vector4 &v);
+
+    constexpr Vector3(float x, float y, float z)
+        : x(x)
+        , y(y)
+        , z(z) {}
 };
 
 struct Vector4 {
     float x, y, z, w;
+
+    Vector4() = default;
+
+    // Yep. Gotta give the w explicitly.
+    constexpr Vector4(const Vector3 &v, float w)
+        : x(v.x)
+        , y(v.y)
+        , z(v.z)
+        , w(w) {}
+
+    constexpr Vector4(float x, float y, float z, float w)
+        : x(x)
+        , y(y)
+        , z(z)
+        , w(w) {}
+
+    Vector4(__m128 pack) { _mm_store_ps(reinterpret_cast<float *>(this), pack); }
 };
+
+constexpr Vector3::Vector3(const Vector4 &v)
+    : x(v.x)
+    , y(v.y)
+    , z(v.z) {}
 
 struct Quaternion {
     float x, y, z, w;
